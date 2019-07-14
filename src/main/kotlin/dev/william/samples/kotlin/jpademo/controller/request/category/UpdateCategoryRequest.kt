11e78a88485aea70a535d5115dc799305ff87ad2
William Johnson dos Santos Okano <williamokano@gmail.com>
2019-07-14 02:10:20 -0300

    Updated category

--- src/main/kotlin/dev/william/samples/kotlin/jpademo/controller/request/category/UpdateCategoryRequest.kt
+++ src/main/kotlin/dev/william/samples/kotlin/jpademo/controller/request/category/UpdateCategoryRequest.kt
@@ -1,5 +1,12 @@
 package dev.william.samples.kotlin.jpademo.controller.request.category
 
+import org.jetbrains.annotations.NotNull
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotEmpty
+
 data class UpdateCategoryRequest(
+    @NotNull
+    @NotEmpty
+    @NotBlank
     val name: String
 )
